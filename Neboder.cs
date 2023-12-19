f5d71ce21f10f3c8a0aea6e67a316d15166a9403
a <145933200+ivasosic@users.noreply.github.com>
2023-12-19 11:19:27 +0100

    Add files via upload

---
+++ Neboder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace neboder
+{
+    internal class Neboder
+    {
+        int brKatova = 0;
+        double visina = 0;
+
+        public int BrKatova { get => brKatova; set => brKatova = value; }
+        public double Visina { get => visina; set => visina = value; }
+
+        public Neboder(int brKatova, double visina)
+        {
+            this.Visina = visina;
+            this.BrKatova = brKatova;
+        }
+        public Neboder()
+        {
+            this.Visina = visina;
+            this.BrKatova = brKatova;
+        }
+
+        public override string ToString()
+        {
+            visina += 10;
+            brKatova += 1;
+            double avgKat = visina / brKatova;
+            string str = "Visina: " + visina + "\nBroj katova: " + brKatova + "\nAvg visina kata: " + avgKat + " metara";
+            return str;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace neboder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Neboder Tower = new Neboder(200, 100);
+
+            Console.WriteLine(Tower.ToString());
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
